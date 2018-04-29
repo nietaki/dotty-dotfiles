5872beea1c9af54f8c9d7c6cd468c19520ba9bb2
Jacek Królikowski <nietaki@gmail.com>
2018-04-29 12:32:15 +0100

    add fish

--- bootstrap/bootstrap_ubuntu.sh
+++ bootstrap/bootstrap_ubuntu.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# TODO maybe change to stable/beta branch once it's out for bionic?
+sudo add-apt-repository ppa:fish-shell/nightly-master
+sudo apt update
+
+
 # yes, no accounting for which directory it got called from
 
 sudo apt install $(< apt_packages.txt)
+
+# other, special cases
+sudo apt install fish
+
+# set fish as the default shell
+chsh -s `which fish`
